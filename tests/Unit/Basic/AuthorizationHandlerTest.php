348429e791732618e5f80abc7df0560b8a5a74dd
Jelmer Schreuder <github@jelmerschreuder.nl>
2025-09-28 11:19:43 +0200

    Got rid of ugly Mockery shorthand

--- tests/Unit/Basic/AuthorizationHandlerTest.php
+++ tests/Unit/Basic/AuthorizationHandlerTest.php
@@ -4,30 +4,29 @@ use jschreuder\MiddleAuth\Basic\AuthorizationHandler;
 use jschreuder\MiddleAuth\AuthorizationMiddlewareInterface;
 use jschreuder\MiddleAuth\AuthorizationRequestInterface;
 use jschreuder\MiddleAuth\AuthorizationResponseInterface;
-use Mockery as m;
 
 describe('Basic\AuthorizationHandler', function () {
     afterEach(function () {
-        m::close();
+        Mockery::close();
     });
 
     it('should throw exception when no handlers are left in stack', function () {
         $stack = new \SplStack();
         $handler = new AuthorizationHandler($stack);
 
-        $handler->handle(m::mock(AuthorizationRequestInterface::class));
+        $handler->handle(Mockery::mock(AuthorizationRequestInterface::class));
     })->throws(\RuntimeException::class, 'No more handlers to call on.');
 
     it('should throw exception when handler is called twice', function () {
         $stack = new \SplStack();
-        $stack->push(m::mock(AuthorizationMiddlewareInterface::class));
-        $stack->push(m::mock(AuthorizationMiddlewareInterface::class));
+        $stack->push(Mockery::mock(AuthorizationMiddlewareInterface::class));
+        $stack->push(Mockery::mock(AuthorizationMiddlewareInterface::class));
 
         $handler = new AuthorizationHandler($stack);
-        $request = m::mock(AuthorizationRequestInterface::class);
+        $request = Mockery::mock(AuthorizationRequestInterface::class);
 
         // First call should work
-        $response = m::mock(AuthorizationResponseInterface::class);
+        $response = Mockery::mock(AuthorizationResponseInterface::class);
         $stack->top()->shouldReceive('process')->once()->andReturn($response);
 
         $handler->handle($request);
@@ -38,16 +37,16 @@ describe('Basic\AuthorizationHandler', function () {
 
     it('should process middleware and return response', function () {
         $stack = new \SplStack();
-        $middleware = m::mock(AuthorizationMiddlewareInterface::class);
+        $middleware = Mockery::mock(AuthorizationMiddlewareInterface::class);
         $stack->push($middleware);
 
         $handler = new AuthorizationHandler($stack);
-        $request = m::mock(AuthorizationRequestInterface::class);
-        $response = m::mock(AuthorizationResponseInterface::class);
+        $request = Mockery::mock(AuthorizationRequestInterface::class);
+        $response = Mockery::mock(AuthorizationResponseInterface::class);
 
         $middleware->shouldReceive('process')
             ->once()
-            ->with($request, m::type(AuthorizationHandler::class))
+            ->with($request, Mockery::type(AuthorizationHandler::class))
             ->andReturn($response);
 
         $result = $handler->handle($request);
@@ -57,18 +56,18 @@ describe('Basic\AuthorizationHandler', function () {
 
     it('should create new handler instance with remaining stack', function () {
         $stack = new \SplStack();
-        $middleware1 = m::mock(AuthorizationMiddlewareInterface::class);
-        $middleware2 = m::mock(AuthorizationMiddlewareInterface::class);
+        $middleware1 = Mockery::mock(AuthorizationMiddlewareInterface::class);
+        $middleware2 = Mockery::mock(AuthorizationMiddlewareInterface::class);
         $stack->push($middleware1);
         $stack->push($middleware2);
 
         $handler = new AuthorizationHandler($stack);
-        $request = m::mock(AuthorizationRequestInterface::class);
-        $response = m::mock(AuthorizationResponseInterface::class);
+        $request = Mockery::mock(AuthorizationRequestInterface::class);
+        $response = Mockery::mock(AuthorizationResponseInterface::class);
 
         $middleware2->shouldReceive('process')
             ->once()
-            ->with($request, m::on(function ($handler) use ($stack, $middleware1) {
+            ->with($request, Mockery::on(function ($handler) use ($stack, $middleware1) {
                 // Verify that the new handler has the remaining stack
                 $reflection = new ReflectionClass($handler);
                 $property = $reflection->getProperty('stack');
