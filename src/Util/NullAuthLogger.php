fdedf401f38c9bd1e1be4bbbf3860666c5ad8884
Jelmer Schreuder <github@jelmerschreuder.nl>
2025-10-31 09:21:30 +0100

    Added comments explaining reason for NullLogger

--- src/Util/NullAuthLogger.php
+++ src/Util/NullAuthLogger.php
@@ -2,17 +2,24 @@
 
 namespace jschreuder\MiddleAuth\Util;
 
+/**
+ * The NullLogger supports disabling logging completely by black-holing all
+ * log entries send to it.
+ */
 final class NullAuthLogger implements AuthLoggerInterface
 {
     public function info(string $message, ?array $context = null): void
     {
+        // does nothing, is a black hole
     }
 
     public function debug(string $message, ?array $context = null): void
     {
+        // does nothing, is a black hole
     }
 
     public function warning(string $message, ?array $context = null): void
     {
+        // does nothing, is a black hole
     }
 }
